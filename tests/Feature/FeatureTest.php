ef33b34c14504e36a1b69b67041d2f9412cd1e45
Oliver <owb4@njit.edu>
2018-07-08 06:59:01 -0400

    Write a feature test to check if your about page returns a 200 status code

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace Tests\Feature;
-
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Database\Seeder;
 
 class FeatureTest extends TestCase
 {
@@ -13,6 +12,7 @@ class FeatureTest extends TestCase
      *
      * @return void
      */
+
     public function testRegister()
     {
         $response = $this->get('/register');
@@ -21,6 +21,17 @@ class FeatureTest extends TestCase
     public function testLogin()
     {
         $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
+    public function testAbout()
+    {
+        //create a user to simulate authentication
+        $user = factory(App\User::class)->create();
+
+        //login and access page using user
+        $response = $this->actingAs($user)
+            ->get('/about');
+
         $response->assertStatus(200);
     }
 }
